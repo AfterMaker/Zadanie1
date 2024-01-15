e69a96c10757c59e1745a9a380a84425e86fa353
SanLa <SanLa@DESKTOP-2JERCE0>
2024-01-15 15:38:34 +0700

    Добавьте файлы проекта.

---
+++ WinFormsApp1/Form1.cs
@@ -0,0 +1,20 @@
+namespace WinFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Группа ИСП-1-20\nАлександр Лагуткин Вячеславович", "Результат:");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
